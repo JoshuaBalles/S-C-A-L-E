24c5f23f209370f998a4df33c98c93528c1f7729
JoshuaBalles <joshuaballes07@gmail.com>
2024-05-14 23:25:22 +0800

    Updated card formatting and added naming.

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-# app.py (do not remove this comment)
+# app.py (do not change/remove this comment)
 from flask import Flask, render_template, request, jsonify, send_from_directory
 import os
 from werkzeug.utils import secure_filename
 import crop
+from datetime import datetime
 
 app = Flask(__name__)
 app.config["UPLOAD_FOLDER"] = "temp"
@@ -25,7 +26,9 @@ def results():
         f for f in os.listdir(app.config["CROPPED_FOLDER"]) if f.endswith(".jpg")
     ]
     image_urls = [image_file for image_file in image_files]
-    return render_template("results.html", image_urls=image_urls)
+    return render_template(
+        "results.html", image_urls=image_urls, get_formatted_date=get_formatted_date
+    )
 
 
 @app.route("/capture_image", methods=["POST"])
@@ -41,15 +44,26 @@ def capture_image():
     filepath = os.path.join(app.config["UPLOAD_FOLDER"], filename)
     file.save(filepath)
 
-    # Call the crop function
-    crop.crop_objects(filepath)
-
-    return jsonify({"success": True, "filepath": filepath})
+    try:
+        # Call the crop function
+        crop.crop_objects(filepath)
+        return jsonify({"success": True, "filepath": filepath})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 
 @app.route("/cropped/<filename>")
 def serve_cropped_image(filename):
     return send_from_directory(app.config["CROPPED_FOLDER"], filename)
+
+
+def get_formatted_date(image_url):
+    parts = image_url.split("_")
+    date_string = parts[0] + " " + parts[1]
+    date_string = date_string[:-6]
+    date_object = datetime.strptime(date_string, "%Y-%m-%d %H-%M-%S")
+    formatted_date = date_object.strftime("%B %d, %Y, %I:%M%p")
+    return formatted_date
 
 
 if __name__ == "__main__":
